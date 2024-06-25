5becc20bf992fa3bf4628ff743910b5f6d13dd99
Alise1982 <Belewich-2010@mail.ru>
2024-06-25 22:22:18 +0300

    add all

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import './App.css';
 import { HW1 } from './HW/1/HW1';
 import { HW2 } from './HW/2/HW2';
@@ -8,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <HW1 />
-      {/*<HW2 />*/}
-      {/*<HW3 />*/}
-      {/*<HW4 />*/}
+      <HW2 />
+      <HW3 />
+      <HW4 />
     </div>
   );
 }
